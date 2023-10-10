74fa0e5eeb272c733399e65cbec13867e9dfc661
Umesh Chandra Karagatla <umeshchandra@Umeshs-MacBook-Air.local>
2023-10-10 18:00:08 -0500

    CSS changes

--- components/contact.tsx
+++ components/contact.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
     
     <motion.section ref={ref} id="contact" className='scroll-mt-28  mb-100 sm:mb-60 w-[min(100%,38rem)]' initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1}}>
         <SectionHeading>Contact me</SectionHeading>
-        <p className="text-gray-700 text-center dark:text-white/80">
+        <p className="text-black text-center">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:uckaragatla@gmail.com">
           uckaragatla@gmail.com
