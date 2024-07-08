258d86f9b6427d4610a7442d7e5f1b56dd859f85
Umesh Chandra Karagatla <umeshchandra@Umeshs-MacBook-Air.local>
2024-07-08 13:36:30 -0500

    Fixing errors in experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -26,7 +26,7 @@ export default function Experience() {
         </li>
         
         <li className="relative flex items-baseline gap-6 pb-5">
-          <div className="before:absolute before:left-[5.5px] before:h-full before:w-[2px] before:bg-gray-400">
+          <div >
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" className="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
               <circle cx="8" cy="8" r="8" />
             </svg>
