8c90ab1597fd0dcdc69dd2180126bd14d19d5288
Saarthak Mudigere Girish <56784459+mudiger@users.noreply.github.com>
2025-06-27 14:11:21 -0500

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -11,6 +11,18 @@ export default function Experience() {
         <SectionHeading>My experience</SectionHeading>
     <div>
       <ul>
+          <li className="relative flex items-baseline gap-6 pb-5">
+          <div className="before:absolute before:left-[5.5px] before:h-full before:w-[2px] before:bg-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" className="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
+              <circle cx="8" cy="8" r="8" />
+            </svg>
+          </div>
+          <div>
+            <p className=" text-gray-500 font-medium">Mar 2025 - Present</p>
+            <p className="mt-2  text-lg font-medium">Software Engineer<br></br>Kaytics</p>
+          </div>
+        </li>
+          
         <li className="relative flex items-baseline gap-6 pb-5">
           <div className="before:absolute before:left-[5.5px] before:h-full before:w-[2px] before:bg-gray-400">
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" className="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
@@ -19,7 +31,7 @@ export default function Experience() {
           </div>
           <div>
             <p className=" text-gray-500 font-medium">May 2024 - Dec 2024</p>
-            <p className="mt-2  text-lg font-medium">Software Developer<br></br>Nucor Corporation</p>
+            <p className="mt-2  text-lg font-medium">Software Engineer<br></br>Nucor Corporation</p>
           </div>
         </li>
         
@@ -30,8 +42,8 @@ export default function Experience() {
             </svg>
           </div>
           <div>
-            <p className=" text-gray-500 font-medium">Oct 2021 - Apr 2022</p>
-            <p className="mt-2  text-lg font-medium">Data Visualization Analyst<br></br>Tata Technologies Limited</p>
+            <p className=" text-gray-500 font-medium">Sep 2021 - Dec 2022</p>
+            <p className="mt-2  text-lg font-medium">Software Engineer<br></br>Dell Technologies</p>
           </div>
         </li>
 
@@ -42,8 +54,8 @@ export default function Experience() {
             </svg>
           </div>
           <div>
-            <p className=" text-gray-500 font-medium">Aug 2021 - Sep 2021</p>
-            <p className="mt-2  text-lg font-medium">UI/UX Designer<br></br>Gustovalley Technovations</p>
+            <p className=" text-gray-500 font-medium">Feb 2021 - Aug 2021</p>                                                                                                                            
+            <p className="mt-2  text-lg font-medium">Software Engineer Intern<br></br>Trigent Software</p>
           </div>
         </li>
       </ul>
